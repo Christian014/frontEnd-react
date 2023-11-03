4ee913ed9d3ab0fa26af91cae9802829f6a2903c
Christian014 <christian.lopes01@hotmail.com>
2023-11-03 18:15:32 -0300

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,14 @@ import { Home } from './pages/home/index'
 import { ThemeProvider } from 'styled-components'
 import theme from './styles/theme'
 import CreateGlobalStyle from './styles/global'
+import { SigIn } from './pages/sigIn'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
 
     <ThemeProvider theme = { theme }>
       <CreateGlobalStyle />
-      <Home/>
+      <SigIn/>
     </ThemeProvider>
 
   </React.StrictMode>,
